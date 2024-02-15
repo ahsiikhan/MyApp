645064cd35ead6cbcf7bbf84480d0a59b463e4bb
ahsiikhan <ahsenikhan@gmail.com>
2024-02-15 17:11:57 +0500

    Updated 15 Feb

--- App.js
+++ App.js
@@ -1,102 +1,42 @@
-import React, { useState, useEffect } from "react";
-import { Image, Pressable, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
-import Header from "./components/Header";
-import AddNewToDo from './components/AddNewToDo';
-import ToDoList from "./components/ToDoList";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Search from "./components/Search";
-
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import CheckBox from './components/CheckBox'
 
 const App = () => {
 
-  const STORAGE_KEY = '@todos';
-
-  const [todos, setTodos] = useState([]);
-  const [searchMode, setSearchMode] = useState(false);
-  const [searchText, setSearchText] = useState('');
-
-  useEffect(() => {
-    loadTodos();
-  }, []);
-
-  useEffect(() => {
-    saveTodos();
-  }, [todos]);
-
-  const loadTodos = async () => {
-    try {
-      const storedTodos = await AsyncStorage.getItem(STORAGE_KEY);
-      if (storedTodos !== null) {
-        setTodos(JSON.parse(storedTodos));
-      }
-    } catch (error) {
-      console.error('Error loading todos: ', error);
-    }
-  }
-
-  const saveTodos = async () => {
-    try {
-      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-    } catch (error) {
-      console.error('Error saving todos: ', error);
-    }
-  }
-
-  const addTodo = (title) => {
-    setTodos(prevTodos => [
-      ...prevTodos,
-      {
-        id: prevTodos.length + 1,
-        title: title,
-        isCompleted: false
-      }
-    ]);
-  }
-
-  const deleteTodo = (id) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    setTodos(updatedTodos);
-  }
-
-  const deleteTodos = (ids) => {
-    const updatedTodos = todos.filter(todo => !ids.includes(todo.id));
-    setTodos(updatedTodos);
-  }
-
+  const [colorsChecked, setColorsChecked] = useState({
+    Red: false,
+    Green: false,
+    Blue: false,
+    Yellow: false,
+  })
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <StatusBar hidden={true} />
-      <Header searchMode={searchMode} setSearchMode={setSearchMode} />
-      <View style={styles.container}>
-        <AddNewToDo addTodo={addTodo} />
-        {searchMode &&
-          <Search searchText={searchText} setSearchText={setSearchText} />
-        }
-        <View style={{ marginTop: 30, marginBottom: searchMode ? 170 : 80 }}>
-          <ToDoList todos={
-            todos.filter((todo) => {
-              for (let key in todo) {
-                if (key === 'title') {
-                  if (todo[key].toLowerCase().includes(searchText.toLowerCase())) {
-                    return true;
-                  }
-                }
-              }
-              return false;
-            })
-          } setTodos={setTodos} deleteTodo={deleteTodo} deleteTodos={deleteTodos} />
-        </View>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Favourite Colors</Text>
+      {Object.entries(colorsChecked).map(([color, value]) => {
+        return (
+          <CheckBox label={color} key={color} isChecked={value} onChange={() => {
+
+            setColorsChecked({ ...colorsChecked, [color]: !colorsChecked[color] })
+
+          }} />
+        )
+      })}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 25,
     flex: 1,
+    backgroundColor: '#fff',
+    padding: 30,
   },
+  heading: {
+    fontFamily: 'Poppins Medium',
+    fontSize: 16,
+  }
 })
 
-export default App;
+export default App
--- components/CheckBox.js
+++ components/CheckBox.js
@@ -1,40 +1,28 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
 
-const Checkbox = ({ isChecked, onChange }) => {
-  return (
-    <TouchableOpacity
-      onPress={() => onChange(!isChecked)}
-    >
-      <View
-        style={{
-          width: 24,
-          height: 24,
-          borderRadius: 20,
-          borderWidth: 1,
-          backgroundColor: isChecked ? '#0AB6AB' : null,
-          borderColor: '#0AB6AB',
-          marginRight: 10,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {isChecked && (
-          <Text
-            style={{
-              fontSize: 15,
-              marginBottom: 4,
-              color: isChecked ? 'white' : 'gray',
-            }}
-          >
-            ✓
-          </Text>
-        )}
-      </View>
-    </TouchableOpacity >
-  );
-};
+const CheckBox = ({ isChecked, onChange, label }) => {
+    return (
+        <TouchableOpacity onPress={() => { onChange(!isChecked) }} style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10, }}>
+            <View
+                style={{
+                    width: 24,
+                    height: 24,
+                    backgroundColor: isChecked ? '#0AB6AB' : '#fff',
+                    borderColor: '#0AB6AB',
+                    borderWidth: 1,
+                    borderRadius: 2,
+                    marginRight: 10,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                {isChecked ? (<Text style={{ fontSize: 15, color: 'white', marginBottom: 4, }}>✔</Text>) : null}
 
+            </View>
+            <Text style={{ fontFamily: 'Poppins Regular', fontSize: 14, }}>{label}</Text>
+        </TouchableOpacity>
+    )
+}
 
-export default Checkbox;
-
+export default CheckBox
